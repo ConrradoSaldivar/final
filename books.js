74e6e4c47049dc50e79f0499deda8df7ed76d651
ConrradoSaldivar <62918963+ConrradoSaldivar@users.noreply.github.com>
2020-06-09 20:44:30 -0600

    added index and about, images, array

---
+++ books.js
@@ -0,0 +1,44 @@
+var Books = [
+	{
+    	fname: "Paul",
+    	lname: "Kalanithi",
+    	Title: "When Breath Becomes Air",
+    	Genre: “Nonfiction”,
+    	Rating: 5
+	},
+	{
+    	fname: "Francisco",
+    	lname: "Cantu",
+    	Title: “The Line Becomes a River”,
+    	Genre: "Nonfiction",
+    	Rating: 5
+	},
+	{
+    	fname: "Hanya",
+    	lname: "Yanagihara",
+    	Title: “A Little Life”,
+    	Genre: "Fiction",
+    	Rating: 5
+	},
+	{
+    	fname: "Thi",
+    	lname: "Bui",
+    	Title: “The Best We Could Do”,
+    	Genre: "Nonfiction",
+    	Rating: 4
+	},
+             {
+    	fname: "Andrew Sean",
+    	lname: "Greer",
+    	Title: “Less”,
+    	Genre: "Fiction",
+    	Rating: 4
+	},
+             {
+    	fname: "Madeline",
+    	lname: "Miller",
+    	Title: “The Song of Achilles”,
+    	Genre: "Fiction",
+    	Rating: 4
+	},
+];
